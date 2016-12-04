d692f0dbe5910b3fc48681a928906b62b0efd121
Hongchang Wu <wuhc85@gmail.com>
2016-12-04 13:31:28 -0500

    Bootstrap vim plugins and color themes (#9)

---
+++ vim/install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+declare -i failed
+
+echo 'Installing vim...'
+
+[[ ! -d "$HOME/.vim" ]] && mkdir -p "$HOME/.vim"
+
+# Pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim || ((failed++))
+
+# grb256
+mkdir -p ~/.vim/colors && curl -LSso ~/.vim/colors/grb256.vim https://raw.githubusercontent.com/garybernhardt/dotfiles/master/.vim/colors/grb256.vim || (failed++)
+
+exit $failed

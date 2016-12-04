3fe1faaf578683a5947db34f872ce81a6dd63a7f
Hongchang Wu <wuhc85@gmail.com>
2016-12-04 16:01:59 -0500

    Use expanded url

--- vim/install.sh
+++ vim/install.sh
@@ -7,7 +7,7 @@ echo 'Installing vim...'
 [[ ! -d "$HOME/.vim" ]] && mkdir -p "$HOME/.vim"
 
 # Pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim || ((failed++))
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim || ((failed++))
 
 # grb256
 mkdir -p ~/.vim/colors && curl -LSso ~/.vim/colors/grb256.vim https://raw.githubusercontent.com/garybernhardt/dotfiles/master/.vim/colors/grb256.vim || ((failed++))

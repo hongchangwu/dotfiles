7d5f35b57824c0def907ff1660ce0b5cdf3f355b
Hongchang Wu <wuhc85@gmail.com>
2017-07-20 21:38:25 -0400

    Fix compatibility issue with fill-column-indicator

--- emacs/site-lisp/init-company.el
+++ emacs/site-lisp/init-company.el
@@ -2,4 +2,13 @@
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
 
+;; Fix compatibility issue with fill-column-indicator
+(defun on-off-fci-before-company(command)
+  (when (string= "show" command)
+    (turn-off-fci-mode))
+  (when (string= "hide" command)
+    (turn-on-fci-mode)))
+
+(advice-add 'company-call-frontends :before #'on-off-fci-before-company)
+
 (provide 'init-company)

c5a1d4dee31476c7c448cc4f46bf2b81c7af96d6
Hongchang Wu <wuhc85@gmail.com>
2017-09-13 20:23:10 -0400

    Use pip to install powerline-shell

--- powerline/install.sh
+++ powerline/install.sh
@@ -11,11 +11,6 @@ echo 'Installing powerline...'
 
 pip install --user powerline-status >/dev/null || ((failed++))
 
-git_clone_or_update https://github.com/banga/powerline-shell.git ~/.local/etc/powerline-shell || ((failed++))
-pushd ~/.local/etc/powerline-shell >/dev/null
-perl -ne 'print unless m/username|hostname/' config.py.dist > config.py 2>/dev/null
-./install.py >/dev/null || ((failed++))
-ln -s ~/.local/etc/powerline-shell/powerline-shell.py ~/.local/bin/powerline-shell.py &>/dev/null
-popd >/dev/null
+pip install --user powerline-shell >/dev/null || ((failed++))
 
 exit $failed

ac0089ee4f9dcffde636a541754d2edc98c1eea7
Hongchang Wu <wuhc85@gmail.com>
2018-09-17 11:13:54 -0400

    Update init-projectile.el

--- emacs/site-lisp/init-projectile.el
+++ emacs/site-lisp/init-projectile.el
@@ -1,6 +1,8 @@
 ;; Projectile
 (require 'projectile)
-(projectile-mode)
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (setq projectile-completion-system 'helm)
 (setq projectile-use-git-grep t)
 (helm-projectile-on)

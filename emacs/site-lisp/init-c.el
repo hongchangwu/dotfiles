e5b4aad4416a0aa80bd7a8af9c5f1cdd775bcdb3
Hongchang Wu <wuhc85@gmail.com>
2019-02-20 17:05:35 -0500

    Update init-c.el

--- emacs/site-lisp/init-c.el
+++ emacs/site-lisp/init-c.el
@@ -1,10 +1,8 @@
 ;; C/C++
 
 (defun my-c-mode-common-hook ()
- (c-set-offset 'substatement-open 0)
-
+ (setq c-default-style "linux"
  (setq c-basic-offset 4)
- (setq c-indent-level 4)
  )
 
 (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)

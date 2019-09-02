b3d824d1b7a5a2411acfc7a916c3ca8843d27f4b
Hongchang Wu <wuhc85@gmail.com>
2019-09-02 13:27:36 -0400

    Fix color and paging in eshell

--- emacs/site-lisp/init-eshell.el
+++ emacs/site-lisp/init-eshell.el
@@ -2,6 +2,7 @@
   :hook
   (eshell-mode . (lambda () (setq show-trailing-whitespace nil)))
   :config
+  (setenv "PAGER" "/bin/cat")
   (eshell-git-prompt-use-theme 'powerline))
 
 (provide 'init-eshell)

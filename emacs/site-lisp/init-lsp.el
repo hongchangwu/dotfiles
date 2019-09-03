238998f52d2e4f5a9eded9cd25dbd0ce9e48c808
Hongchang Wu <wuhc85@gmail.com>
2019-09-03 09:26:53 -0400

    Add missing init-lsp.el

---
+++ emacs/site-lisp/init-lsp.el
@@ -0,0 +1,25 @@
+(use-package lsp-mode
+  :commands lsp)
+
+(use-package lsp-ui
+  :commands lsp-ui-mode
+  :hook
+  (lsp-mode . lsp-ui-mode)
+  :bind
+  (:map lsp-ui-mode-map
+        ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
+        ([remap xref-find-references] . lsp-ui-peek-find-references)))
+
+(use-package company-lsp
+  :commands company-lsp
+  :config
+  (push 'company-lsp company-backends))
+
+(use-package helm-lsp
+  :commands helm-lsp-workspace-symbol
+  )
+
+(use-package lsp-treemacs
+  :commands lsp-treemacs-errors-list)
+
+(provide 'init-lsp)

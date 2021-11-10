dbc60de431c051f21f68f61a5a52a7e229150351
Hongchang Wu <wuhc85@gmail.com>
2021-11-09 22:55:24 -0500

    Enable tree-sitter for OCaml

--- nixpkgs/emacs/site-lisp/init-tree-sitter.el
+++ nixpkgs/emacs/site-lisp/init-tree-sitter.el
@@ -2,12 +2,11 @@
 
 ;;; Commentary:
 
+;; Tree-sitter is a parser generator tool and an incremental parsing library.
+
 ;;; Code:
 
-(use-package tree-sitter
-  :config
-  (global-tree-sitter-mode))
-
+(use-package tree-sitter)
 (use-package tree-sitter-langs)
 
 (provide 'init-tree-sitter)

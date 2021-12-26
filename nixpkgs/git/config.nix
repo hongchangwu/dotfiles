a2f2aea1e0132fe2b11fa73f1bea5fd51a6554e5
Hongchang Wu <wuhc85@gmail.com>
2021-12-26 15:03:23 -0500

    Reorder git aliases

--- nixpkgs/git/config.nix
+++ nixpkgs/git/config.nix
@@ -40,32 +40,32 @@
   };
 
   alias = {
-    co = "checkout";
-    cob = "checkout -b";
-    cot = "checkout -t";
     a = "add";
     aa = "add -A";
+    abandon = ''!git add -A && git commit -qm "Abandoned" && git reset --hard HEAD~'';
+    amend = "commit -a --amend";
     au = "add -u";
+    br = "!git --no-pager branch";
     c = "commit -m";
     ca = "!git add -A && git commit -m";
     cf = "commit --fixup";
+    co = "checkout";
+    cob = "checkout -b";
+    cot = "checkout -t";
+    cp = "cherry-pick";
     cu = "!git add -u && git commit -m";
-    wip = ''commit -am "WIP"'';
-    undo = "reset --mixed HEAD~";
     extend = "commit -a --amend --no-edit";
-    amend = "commit -a --amend";
-    reword = "commit --amend";
-    abandon = ''!git add -A && git commit -qm "Abandoned" && git reset --hard HEAD~'';
     l = "!git --no-pager log --oneline --decorate HEAD";
-    lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit HEAD";
-    lu = "!git lg -u HEAD";
     last = "!git --no-pager show --name-only --oneline HEAD";
-    st = "status --short -uno";
+    lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit HEAD";
     ls = "ls-files";
+    lu = "!git lg -u HEAD";
+    review = "diff --cached";
+    reword = "commit --amend";
+    st = "status --short -uno";
     sync-merge = "!git fetch upstream master && git merge FETCH_HEAD";
     sync-rebase = "!git fetch upstream master && git rebase FETCH_HEAD";
-    review = "diff --cached";
-    cp = "cherry-pick";
-    br = "!git --no-pager branch";
+    undo = "reset --mixed HEAD~";
+    wip = ''commit -am "WIP"'';
   };
 }

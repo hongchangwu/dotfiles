d32fd60f4c20d77647aaa09369d12e6be166b4c9
Hongchang Wu <wuhc85@gmail.com>
2022-01-01 02:17:39 +0000

    Add flake.nix

---
+++ nixpkgs/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    home-manager.url = "github:nix-community/home-manager/release-21.11";
+  };
+
+  outputs = { home-manager, nixpkgs, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.hongchangwu = import ./home.nix;
+
+          # Optionally, use home-manager.extraSpecialArgs to pass
+          # arguments to home.nix
+        }
+      ];
+    };
+  };
+}

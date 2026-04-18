d75c6c111b3857a8cd3c7f59dc802cb74728d676
Hongchang Wu <wuhc85@gmail.com>
2026-04-18 08:18:43 +0800

    NixOS 25.11

--- install.sh
+++ install.sh
@@ -2,16 +2,19 @@
 
 set -euo pipefail
 
+NIX_VERSION="2.22.0"
+NIXOS_VERSION="25.11"
+
 # Install Nix
-sh <(curl -L https://releases.nixos.org/nix/nix-2.17.0/install) --daemon --no-channel-add
-nix-channel --add https://nixos.org/channels/nixos-23.05 nixpkgs
+sh <(curl -L "https://releases.nixos.org/nix/nix-${NIX_VERSION}/install") --daemon --no-channel-add
+nix-channel --add "https://nixos.org/channels/nixos-${NIXOS_VERSION}" nixpkgs
 
 DIR=$(dirname "$(readlink -f "$0")")
 mkdir -p "$HOME/.config/"
 [[ ! -d "$HOME/.config/home-manager" ]] && ln -s "$DIR/home-manager/" "$HOME/.config/home-manager"
 
 # Install home-manager
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
+nix-channel --add "https://github.com/nix-community/home-manager/archive/release-${NIXOS_VERSION}.tar.gz" home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
 
